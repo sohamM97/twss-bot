12324c3f2aa44fadf6bbc4a0b7ade74f34a2ea37
sohamM97 <soham.marik@gmail.com>
2021-01-31 20:58:37 +0530

    Discord bot

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import os
 
 import discord
+import joblib
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,8 +10,21 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+model = joblib.load('twss_model.sav')
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+	print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+
+	text=message.content
+	prediction=model.predict([text])[0]
+	if prediction==1:
+		response = "That's what she said!"
+		await message.channel.send(response)
 
 client.run(TOKEN)
